42c4f9b512efe47d1145d6697ce4c842b850e424
Fumiya Sakai <just1factory@gmail.com>
2018-11-25 22:24:53 +0900

    Initial Commit

---
+++ RxSwiftUIExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftUIExample
+//
+//  Created by 酒井文也 on 2018/11/25.
+//  Copyright © 2018 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
