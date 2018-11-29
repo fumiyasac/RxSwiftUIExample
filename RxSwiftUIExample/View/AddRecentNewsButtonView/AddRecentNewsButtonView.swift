58d55889a3117b8d3ddb22ff0626b46373e79a64
Fumiya Sakai <just1factory@gmail.com>
2018-11-29 17:39:41 +0900

    デザイン調整

---
+++ RxSwiftUIExample/View/AddRecentNewsButtonView/AddRecentNewsButtonView.swift
@@ -0,0 +1,23 @@
+//
+//  AddRecentNewsButtonView.swift
+//  RxSwiftUIExample
+//
+//  Created by 酒井文也 on 2018/11/29.
+//  Copyright © 2018 酒井文也. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddRecentNewsButtonView: CustomViewBase {
+
+    // MARK: - Initializer
+
+    required init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
