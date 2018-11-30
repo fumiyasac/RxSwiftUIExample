c03e80c5040e1a6b1007bdece64988befd161ffd
Fumiya Sakai <just1factory@gmail.com>
2018-11-30 13:00:06 +0900

    Toast表示を試してみる

--- RxSwiftUIExample/ViewController/NewsWebPage/NewsWebPageViewController.swift
+++ RxSwiftUIExample/ViewController/NewsWebPage/NewsWebPageViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 import WebKit
+import Toast_Swift
 
 // MEMO: DeckTransitionを使用した表示でスワイプが強いと戻ってしまう...
 class NewsWebPageViewController: UIViewController {
-    
+
     @IBOutlet weak private var headerBackgroundView: UIView!
 
     override func viewDidLoad() {
@@ -20,12 +21,17 @@ class NewsWebPageViewController: UIViewController {
         setupUserInterface()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
     // MARK: - Function
 
     // MARK: - Private Function
 
     private func setupUserInterface() {
         setupNewsWebview()
+        setupToastForAnnounce()
     }
     
     private func setupNewsWebview() {
@@ -54,5 +60,22 @@ class NewsWebPageViewController: UIViewController {
             let urlRequest = URLRequest(url: url)
             webView.load(urlRequest)
         }
+    }
+
+    private func setupToastForAnnounce() {
+
+        // TODO: 後で正しい形に直す
+        let space: CGFloat = 78.0
+        let centerX = UIScreen.main.bounds.width / 2
+        let centerY = UIScreen.main.bounds.height - space
+        let toastShowPoint = CGPoint(x: centerX, y: centerY)
+
+        var style = ToastStyle()
+        style.messageFont = UIFont(name: AppConstant.COMMON_FONT_BOLD, size: AppConstant.COMMON_NAVIGATION_FONT_SIZE)!
+        style.messageColor = .white
+        style.messageAlignment = .center
+        style.backgroundColor = UIColor(code: "#333333", alpha: 0.5)
+
+        self.view.makeToast("This is Toast Example!", point: toastShowPoint, title: nil, image: nil, completion: nil)
     }
 }
