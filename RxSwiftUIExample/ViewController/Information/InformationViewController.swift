269ee2f1098d67795fe5b3b8a3ad7f8d5db4956d
Fumiya Sakai <just1factory@gmail.com>
2018-12-05 15:00:30 +0900

    Information.storyboardに対応する画面を作る

--- RxSwiftUIExample/ViewController/Information/InformationViewController.swift
+++ RxSwiftUIExample/ViewController/Information/InformationViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class InformationViewController: UIViewController {
 
+    private let originalInformationTopImageHeight: CGFloat = 240
+
+    @IBOutlet weak private var informationScrollView: UIScrollView!
+    @IBOutlet weak private var informationTopImageView: UIImageView!
+
+    // TOP画像において変更対象となるAutoLayoutの制約値
+    @IBOutlet weak var informationTopImageHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var informationTopImageTopConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,5 +29,27 @@ class InformationViewController: UIViewController {
 
     private func setupUserInterface() {
         setupNavigationBar(title: "Information")
+        setupInformationScrollView()
+        setupInformationTopImageView()
+    }
+
+    private func setupInformationScrollView() {
+
+        // NavigationBar分のスクロール位置がずれてしまう考慮を下記のように行う
+        // 考慮する項目1. Information.storyboardにおいて「Adjust Scroll View Insets」のチェックを外す
+        // 考慮する項目2. informationScrollViewのTopのAutoLayoutを「Information Scroll View.top = SafeArea.top」とする
+        informationScrollView.delegate = self
+    }
+
+    private func setupInformationTopImageView() {
+        informationTopImageHeightConstraint.constant = originalInformationTopImageHeight
+    }
+}
+
+extension InformationViewController: UIScrollViewDelegate {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        informationTopImageTopConstraint.constant = min(scrollView.contentOffset.y, 0)
+        informationTopImageHeightConstraint.constant = max(0, originalInformationTopImageHeight - scrollView.contentOffset.y)
     }
 }
